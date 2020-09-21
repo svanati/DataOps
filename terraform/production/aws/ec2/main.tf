b5d924da3c5e7157720ecde6ad05230917c6b355
Elvin Ellis Smith Jr <svanati@gmail.com>
2020-09-21 11:51:20 -0400

    Updated the configuration files

--- terraform/production/aws/ec2/main.tf
+++ terraform/production/aws/ec2/main.tf
@@ -2,5 +2,5 @@ module "ec2" {
     source = "git::https://github.com/svanati/DataOps.git//terraform/modules/aws/ec2?ref=master"
     aws_instance_name = "airflow-us"
     aws_region = "us-east-1"
-    ssh_pem_file = "~/.ssh/AWS-DevOps.pem"
+    ssh_pem_file = "AWS-DevOps"
 }
