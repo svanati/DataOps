19bbfd3e806279b11e23115833e7083e9da66000
svanati@gmail.com <svanati@gmail.com>
2020-09-23 06:41:28 -0400

    Updated main.tf

--- terraform/staging/aws/ec2/main.tf
+++ terraform/staging/aws/ec2/main.tf
@@ -3,6 +3,9 @@ module "ec2" {
     aws_instance_name        = "airflow-us-stage"
     aws_instance_type        = "t3.micro"
     aws_region               = "us-east-1"
+    aws_security_app_port    = 8080
     aws_security_cidr_blocks = "0.0.0.0/0"
+    aws_security_group       = "airflow"
+    aws_service_name         = "airflow"
     ssh_pem_file             = "AWS-DevOps"
 }
