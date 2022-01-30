12b275897e37c3eb9c5e80bea9567709643e1370
Elvin Ellis Smith Jr <svanati@users.noreply.github.com>
2022-01-30 11:17:35 -0500

    Uploaded Kafka (Confluent) CLI Manager code
    Uploaded Kafka (Confluent) CLI Manager code

---
+++ Streaming/Kafka/Confluent/Cloud CLI Manager/py-ccloud-cli-manager.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""Confluent Cloud CLI MAnager
+
+This module provides a simple way of managing a Confluent Cloud system via the
+CLI API command(s).
+
+Example:
+        $ python py-ccloud-cli-worker
+
+License:
+        MIT
+
+Attributes:
+    title_row_index (int) This variable stores the index title row value.
+
+    required_rows_length (int) This variable stores the index required rows
+    value.
+
+    env_login_email (string) This variable retrieves and stores the Confluent
+    Cloud login email address value.
+
+    env_login_password (string)
+ENV_LOGIN_PASSWORD = "CCLOUD_PASSWORD"
+    module_level_variable1 (int): Module level variables may be documented in
+        either the ``Attributes`` section of the module docstring, or in an
+        inline docstring immediately following the variable.
+
+        Either form is acceptable, but the two should not be mixed. Choose
+        one convention to document module level variables and be consistent
+        with it.
+
+Todo:
+    * For module TODOs
+    * You have to also use ``sphinx.ext.todo`` extension
+
+.. _Google Python Style Guide:
+   http://google.github.io/styleguide/pyguide.html
+
+"""
